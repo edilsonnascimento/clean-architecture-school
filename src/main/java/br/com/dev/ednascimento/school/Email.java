eb7ef4d56a9517f9fb7be7248b20b45d4dad8b94
Edilson do Nascimento <edilson.nascimento@nexti.com>
2023-12-05 06:34:44 -0300

    02 - ADDED domain classes.

---
+++ src/main/java/br/com/dev/ednascimento/school/Email.java
@@ -0,0 +1,4 @@
+package br.com.dev.ednascimento.school;
+
+public class Email {
+}
---
+++ src/main/java/br/com/dev/ednascimento/school/student.java
@@ -0,0 +1,8 @@
+package br.com.dev.ednascimento.school;
+
+public class student {
+
+    private String cpf;
+    private String name;
+    private Email email;
+}
