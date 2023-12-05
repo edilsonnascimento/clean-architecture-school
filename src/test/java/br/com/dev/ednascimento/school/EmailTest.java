c064e3942be1cc46f2b675c12ff845ea8e617df4
Edilson do Nascimento <edilson.nascimento@nexti.com>
2023-12-05 08:54:35 -0300

    04 - ADDED unit test for domain email.

---
+++ src/test/java/br/com/dev/ednascimento/school/EmailTest.java
@@ -0,0 +1,32 @@
+package br.com.dev.ednascimento.school;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class EmailTest {
+
+    @ParameterizedTest
+    @MethodSource("inValidDataProvider")
+    void GIVEN_invalid_email_THEN_throws_Expcetion(String address) {
+
+        // Given
+        var expected = "E-mail inválido!";
+
+        // When
+        var actual = assertThrows(IllegalArgumentException.class, () -> new Email(address));
+
+        // Then
+        assertThat(actual.getMessage()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> inValidDataProvider() {
+        return Stream.of(arguments(""), arguments("ivalidEmail"), null);
+    }
+
+}
