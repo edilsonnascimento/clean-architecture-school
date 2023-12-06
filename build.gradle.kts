2a3d603644a3e3c6af2c3e5736246f5b6614ad36
Edilson do Nascimento <edilson.nascimento@nexti.com>
2023-12-06 18:00:44 -0300

    11 - ADDED student repository implementation in JDBC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "br.com.dev.ednascimento"
@@ -10,6 +12,11 @@ repositories {
 }
 
 dependencies {
+
+    // Implementation
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+    // Test
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation ("org.assertj:assertj-core:3.21.0")
---
+++ src/main/java/br/com/dev/ednascimento/school/infra/school/StudentRepositoryJDBCImpl.java
@@ -0,0 +1,67 @@
+package br.com.dev.ednascimento.school.infra.school;
+
+import br.com.dev.ednascimento.school.domain.student.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.*;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.Optional.of;
+
+@Repository
+public class StudentRepositoryJDBCImpl implements StudentRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Override
+    public void register(Student student) {
+        var sql =
+                  """
+                  INSERT INTO student(national_identification, name, email)
+                  VALUES(:national_identification,:name, :email)
+                  """;
+        var parameters = new MapSqlParameterSource()
+                .addValue("national_identification", student.getNationalIdentification().getCPF())
+                .addValue("name", student.getName())
+                .addValue("email", student.getEmail().getAddress());
+        jdbcTemplate.update(sql, parameters);
+    }
+
+    @Override
+    public Optional<Student> findToNationalIdentification(NationalIdentification nationalIdentification) {
+        var sql =
+                """
+                SELECT national_identification, name, email
+                FROM student
+                WHERE national_identification = :national_identification
+                """;
+        var parameters = new MapSqlParameterSource()
+                .addValue("national_identification", nationalIdentification);
+        return returnStream(sql, parameters).findFirst();
+    }
+
+    @Override
+    public Optional<List<Student>> findAllRegisters() {
+        var sql =
+                """
+                SELECT national_identification, name, email
+                FROM student
+                """;
+        return of(returnStream(sql, new MapSqlParameterSource()).toList());
+    }
+
+    private Stream<Student> returnStream(String sql, MapSqlParameterSource parameters) {
+        return jdbcTemplate.query(sql, parameters, StudentRepositoryJDBCImpl::mapRow).stream();
+    }
+
+    private static Student mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        return new Student(
+                new NationalIdentification(resultSet.getString("national_identification")),
+                resultSet.getString("name"),
+                new Email(resultSet.getString("email")));
+    }
+}
