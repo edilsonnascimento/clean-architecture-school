88b900d2fcdc84be5594237353358acd33d0eac7
Edilson do Nascimento <edilson.nascimento@nexti.com>
2023-12-07 05:52:35 -0300

    12 - ADDED student repository implementation in Memory

---
+++ src/main/java/br/com/dev/ednascimento/school/infra/school/StudentRepositoryMemoryImpl.java
@@ -0,0 +1,27 @@
+package br.com.dev.ednascimento.school.infra.school;
+
+import br.com.dev.ednascimento.school.domain.student.*;
+
+import java.util.*;
+
+public class StudentRepositoryMemoryImpl implements StudentRepository {
+
+    private List<Student> studentsEnrolled = new ArrayList<>();
+
+    @Override
+    public void register(Student student) {
+        studentsEnrolled.add(student);
+    }
+
+    @Override
+    public Optional<Student> findToNationalIdentification(NationalIdentification nationalIdentification) {
+        return studentsEnrolled.stream()
+                .filter(student -> student.getNationalIdentification().getCPF().equals(nationalIdentification.getCPF()))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<List<Student>> findAllRegisters() {
+        return Optional.of(studentsEnrolled);
+    }
+}
